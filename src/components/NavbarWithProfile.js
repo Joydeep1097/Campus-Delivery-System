d2a42752742d7f52e5603f5888cfe3fd36e64067
chayan07 <pathakchayan20@gmail.com>
2024-01-15 23:23:45 +0530

    frontend two

--- src/components/NavbarWithProfile.js
+++ src/components/NavbarWithProfile.js
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom';
 
 const NavbarWithProfile = ({ accountId, onLogout }) => {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
+    <nav className="navbar1">
+      <div className="navbar1-logo">
         <Link to="/">Campus Bazar</Link>
       </div>
-      <div className="navbar-items">
-        <div className="navbar-profile-info">
+      <div className="navbar1-items">
+        <div className="navbar1-profile-info">
           <span>Account ID: {accountId}</span>
           <Link to="/orders">Orders</Link>
         </div>
