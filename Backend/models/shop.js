ad746b8670aff0a0c469547cfbc6e6daf7f631bd
bkdhorajiya <112803763+bkdhorajiya@users.noreply.github.com>
2024-01-24 20:19:57 +0530

    Added Shop Registration

---
+++ Backend/models/shop.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const shopSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 255,
+    },
+    shopDescription : {
+        type : String,
+    },
+    image: { 
+        type:String, 
+    },
+    addressId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Address', 
+        required: true 
+    },
+    category: [ 
+        { 
+            type:mongoose.Schema.Types.ObjectId, 
+            ref:"Category", 
+        } 
+    ], 
+});
+
+module.exports = mongoose.model('Shop', shopSchema);
