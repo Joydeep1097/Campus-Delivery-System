67fa878a7dfa98159c04224889f19bf2e81f0693
bkdhorajiya <112803763+bkdhorajiya@users.noreply.github.com>
2024-01-29 15:38:47 +0530

    updated routes

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,16 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 
-const {userSignup, userLogin } = require("../Controllers/Auth");
-const {vendorSignup, vendorLogin} = require("../Controllers/vendorAuth");
-const {vendorCategory, vendorGetCategory, vendorAddProduct, vendorDeleteItem, vendorUpdateCategoryName,vendorUpdateProductDetail} = require("../Controllers/vendor");
+const { userSignup, userLogin } = require("../Controllers/Auth");
+const { vendorSignup, vendorLogin} = require("../Controllers/vendorAuth");
+const { userGetShopCProducts } = require("../Controllers/user");
 
+const {vendorCategory, vendorGetCategory, vendorAddProduct, vendorDeleteItem, vendorUpdateCategoryName,vendorUpdateProductDetail} = require("../Controllers/vendor");
 
 
 const upload = require("../middlewares/multer");
 
 router.post("/login",userLogin);
 router.post("/signup", userSignup);
+router.post("/shop", userGetShopCProducts);
+
 router.get("/allProductperCategoryShop",vendorGetCategory);
 //router.post("/ChangePassword", userchangePassword); 
 //router.post("/getShopList", getShopList);   
@@ -25,11 +28,7 @@ router.get("/allProductperCategoryShop",vendorGetCategory);
 router.post("/vendor/login",vendorLogin);
 router.post("/vendor/signup",upload.single("image"), vendorSignup);
 router.post("/vendor/addcategoryproduct",vendorCategory);
-
-
-router.post("/vendor/add-product",vendorAddProduct);
-
-
+router.post("/vendor/add-product",vendorAddProduct);  //given the category name
 router.delete("/vendor/delete-item",vendorDeleteItem);
 router.post("/vendor/update-category-name",vendorUpdateCategoryName);
 router.post("/vendor/update-product-details",vendorUpdateProductDetail);
