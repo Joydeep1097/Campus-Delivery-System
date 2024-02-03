18dc657a1c0a5ee9ad14740a5a64989e2da5cedf
chayan07 <pathakchayan20@gmail.com>
2024-02-04 02:02:46 +0530

    forntend updates

--- Frontend/src/components/UserShopPage.js
+++ Frontend/src/components/UserShopPage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-
+import Cart from './Cart';
 const UserShopPage = (props) => {
   const [shop, setShop] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
- 
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+  const [currentPagecategory, setCurrentPagecategory] = useState(1);
+  const itemsPerPagecategory = 5;
   useEffect(() => {
     const fetchShopData = async () => {
       const utoken = localStorage.getItem("token");
@@ -19,6 +21,8 @@ const UserShopPage = (props) => {
         });
         const data = await response.json();
         setShop(data.shop.shop);
+        console.log("hiii")
+
       } catch (error) {
         console.error('Error fetching shops:', error);
       }
@@ -27,16 +31,31 @@ const UserShopPage = (props) => {
     fetchShopData();
   }, [props.id]);
 
+  // Calculate the index range for the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  // Function to change the current page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const indexOfLastItemcategory = currentPagecategory * itemsPerPagecategory;
+  const indexOfFirstItemcategory = indexOfLastItemcategory - itemsPerPagecategory;
+  // Function to change the current page
+  const paginatecategory = (pageNumbercategory) => setCurrentPagecategory(pageNumbercategory);
   return (
     <div>
       <div className="side-panel">
         <header>
           <h1>{shop.name}</h1>
         </header>
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button type="button">🔍Search</button>
+        </div>
+        <Cart />
         <div>
           <h3>Categories</h3>
-          <select value={selectedCategory} onChange={(e) => { if (e.target.value !== "Select a Category") { setSelectedCategory(e.target.value) } }}>
-            <option value={null}>Select a Category</option>
+          <select value={selectedCategory} onChange={(e) => { if (e.target.value !== "All") { setSelectedCategory(e.target.value); paginate(1) } else { setSelectedCategory(null); paginate(1) } }}>
+            <option value={null}>All</option>
             {shop.categories?.map((category) => (
               <option key={category.id} value={category.id}>
                 {category.name}
@@ -45,33 +64,53 @@ const UserShopPage = (props) => {
           </select>
         </div>
       </div>
+      <div className="pagination">
+        <p>Pages</p>
+        {Array.from({ length: Math.ceil(shop.categories?.length / itemsPerPagecategory) }, (_, index) => index + 1).map((pageNumbercategory) => (
+          <button2 key={pageNumbercategory} onClick={() => paginatecategory(pageNumbercategory)}>
+            {pageNumbercategory}
+          </button2>
+        ))}
+      </div>
       <div className='page'>
         <main>
           {selectedCategory && (
             <>
               <div>
                 <h4>{shop.categories.find((c) => c.id === selectedCategory)?.name}</h4>
-                <div className="product-list">
-                {shop.categories.find((c) => c.id === selectedCategory)?.products.map((product) => (
-                    <div key={product.id} className="product-card">
-                      <h5>{product.name}</h5>
-                      <p>Rs.{product.price}</p>
-                    </div>
+                <div className="pagination">
+                  <p>Pages</p>
+                  {Array.from({ length: Math.ceil(shop.categories.find((c) => c.id === selectedCategory)?.products.length / itemsPerPage) }, (_, index) => index + 1).map((pageNumber) => (
+                    <button2 key={pageNumber} onClick={() => paginate(pageNumber)}>
+                      {pageNumber}
+                    </button2>
                   ))}
+                </div>
+                <div className="product-list">
+                  {shop.categories.find((c) => c.id === selectedCategory)?.products
+                    .slice(indexOfFirstItem, indexOfLastItem)
+                    .map((product) => (
+                      <div key={product.id} className="product-card" onClick={() => console.log(product.id)} >
+                        <h5>{product.name}</h5>
+                        <p>Rs.{product.price}</p>
+                      </div>
+                    ))}
                 </div>
               </div>
             </>
           )}
-          {!selectedCategory && shop.categories?.map((category) => (
+          {!selectedCategory && shop.categories?.slice(indexOfFirstItemcategory, indexOfLastItemcategory).map((category) => (
             <div key={category.id}>
               <h2>{category.name}</h2>
               <div className='product-list'>
-                {category.products.map((product) => (
-                  <div key={product.id} className="product-card">
-                    <h3>{product.name}</h3>
-                    <span className="price">Rs.{product.price}</span>
-                  </div>
-                ))}
+                {category.products
+                  .slice(indexOfFirstItem, indexOfLastItem)
+                  .map((product) => (
+                    <div key={product.id} className="product-card" onClick={() => console.log(product.id)}>
+                      <h3>{product.name}</h3>
+                      <span className="price">Rs.{product.price}</span>
+                    </div>
+                  ))}
               </div>
             </div>
           ))}
