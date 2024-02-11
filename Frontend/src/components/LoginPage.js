f6b0a52c6fc7d1beb95aa708532781061675c62f
chayan07 <pathakchayan20@gmail.com>
2024-02-11 20:08:07 +0530

    forntend updates

--- Frontend/src/components/LoginPage.js
+++ Frontend/src/components/LoginPage.js
@@ -1,6 +1,6 @@
 // LoginPage.js
 
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import UserProfile from './UserProfile';
 
@@ -8,19 +8,42 @@ const LoginPage = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [flag, setflag] = useState('');
-  const [name,setName] = useState('');
+  const [name, setName] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  useEffect(()=>{
-    if(localStorage.getItem('token')){
-      setflag(localStorage.getItem('token'));
-      setName(localStorage.getItem('name'));
+  useEffect(() => {
+    const validate = async () => {
+      if (localStorage.getItem('token')) {
+        console.log("got");
+        const utoken = localStorage.getItem("token");
+        try {
+          
+          const response = await fetch('http://localhost:27017/api/v1/validateTokenUser', {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${utoken}` }
+          });
+          const data = await response.json();
+          console.log(data)
+          if(data.success==true){
+            setflag(localStorage.getItem('token'));
+          setName(localStorage.getItem('name'));
+          }
+        }
+        catch (error) {
+          console.error('Error fetching shops:', error);
+        } finally {
+          //setLoading(false);
+        }
+      };
+      
     }
-  },[]);
+    validate();
+  }, []);
   const handleLogin = async () => {
     // ValEmailate Email format (same as during signup)
     if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
       alert('Email must be in the format digits@alphabetical.alphabetical');
-      return;}
+      return;
+    }
 
     // Implement login logic here
     //console.log(name);
@@ -36,23 +59,23 @@ const LoginPage = (props) => {
       }
       //console.log(JSON.stringify(data));
       const response = await fetch('http://localhost:27017/api/v1/login', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const result = await response.json();
+      //console.log(result);
+      if (result.success === true) {
+        localStorage.setItem('token', result.token);
+        localStorage.setItem('name', result.user.name);
+        setflag(result.token);
+        setName(result.user.name);
       }
-    });
-    const result = await response.json();
-    console.log(result);
-    if(result.success===true){
-      localStorage.setItem('token',result.token);
-      localStorage.setItem('name',result.user.name);
-      setflag(result.token);
-      setName(result.user.name);
-    }
-    if(result.success===false){
-      alert(result.message);
-    }
+      if (result.success === false) {
+        alert(result.message);
+      }
     } catch (error) {
       // Handle errors (show an error message to the user)
       console.error(error.response.data);
@@ -61,56 +84,56 @@ const LoginPage = (props) => {
 
   return (
     <div>
-    {flag!==''?<UserProfile name={name}/>:
-    <div className="outer">
-    <div className='container'>
-      <img src="images/user.png" alt="" className='userimage2' />
-      <h1>Login</h1>
-      <form>
-        <label>
-          <input
-            type="email"
-            value={email}
-            placeholder='📧Email'
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          <input
-            type={showPassword ? 'text' : 'password'}
-            value={password}
-            placeholder='🔑Password'
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <div className="show-password">
-            <input
-              type="checkbox"
-              id="showPassword"
-              checked={showPassword}
-              onChange={() => setShowPassword(!showPassword)}
-            />
-            <label htmlFor="showPassword">Show Password</label>
+      {flag !== '' ? <UserProfile name={name} /> :
+        <div className="outer">
+          <div className='container'>
+            <img src="images/user.png" alt="" className='userimage2' />
+            <h1>Login</h1>
+            <form>
+              <label>
+                <input
+                  type="email"
+                  value={email}
+                  placeholder='📧Email'
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+              <br />
+              <label>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  value={password}
+                  placeholder='🔑Password'
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+              <br />
+              <div className="show-password">
+                <input
+                  type="checkbox"
+                  id="showPassword"
+                  checked={showPassword}
+                  onChange={() => setShowPassword(!showPassword)}
+                />
+                <label htmlFor="showPassword">Show Password</label>
+              </div>
+              <br />
+              <div className="button-container">
+                <button type="button" className="login-button" onClick={handleLogin}>
+                  Login
+                </button>
+
+                <Link to="/">
+                  <button type="button" className="back-button">
+                    Back
+                  </button>
+                </Link>
+              </div>
+            </form>
           </div>
-        <br />
-        <div className="button-container">
-          <button type="button" className="login-button" onClick={handleLogin}>
-            Login
-          </button>
-        
-        <Link to="/">
-          <button type="button" className="back-button">
-            Back
-          </button>
-        </Link>
         </div>
-      </form>
+      }
     </div>
-    </div>
-  }
-  </div>
   );
 };
 
--- Frontend/src/components/UserShopPage.js
+++ Frontend/src/components/UserShopPage.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Cart from './Cart';
 import ShowProduct from './ShowProduct';
 const UserShopPage = (props) => {
   const [shop, setShop] = useState([]);
-  const [product,setProduct] = useState([]);
+  const [product, setProduct] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 4;
   const [currentPagecategory, setCurrentPagecategory] = useState(1);
   const itemsPerPagecategory = 5;
+  const productchoose = useCallback(() => {
+    setSelectedCategory(selectedCategory);
+  }, [setSelectedCategory]);
   useEffect(() => {
     const fetchShopData = async () => {
       const utoken = localStorage.getItem("token");
@@ -36,96 +39,105 @@ const UserShopPage = (props) => {
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   // Function to change the current page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  console.log(product)
+  console.log("hii", product)
+  const goBack = () => {
+    // Implement logic to add the product to the cart
+    setProduct([]);
+  };
   const indexOfLastItemcategory = currentPagecategory * itemsPerPagecategory;
   const indexOfFirstItemcategory = indexOfLastItemcategory - itemsPerPagecategory;
   // Function to change the current page
   const paginatecategory = (pageNumbercategory) => setCurrentPagecategory(pageNumbercategory);
   return (
     <>
-    {product.id?<ShowProduct product={product}/>:
-    <div>
-      <div className="side-panel">
-        <header>
-          <h1>{shop.name}</h1>
-        </header>
-        <div className="search-bar">
-          <input type="text" placeholder="Search..." />
-          <button type="button">🔍Search</button>
+      <div>
+        <div className="side-panel">
+          <header>
+            <h1>{shop.name}</h1>
+          </header>
+          <div className="search-bar">
+            <input type="text" placeholder="Search..." />
+            <button type="button">🔍Search</button>
+          </div>
+          <Cart count={ShowProduct.count} />
+          <div>
+            <h3>Categories</h3>
+            <select value={selectedCategory} onChange={(e) => { if (e.target.value !== "All") { setSelectedCategory(e.target.value); paginate(1); setProduct([]) } else { setSelectedCategory(null); paginate(1); setProduct([]) } }}>
+              <option value={null}>All</option>
+              {shop.categories?.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <Cart />
         <div>
-          <h3>Categories</h3>
-          <select value={selectedCategory} onChange={(e) => { if (e.target.value !== "All") { setSelectedCategory(e.target.value); paginate(1) } else { setSelectedCategory(null); paginate(1) } }}>
-            <option value={null}>All</option>
-            {shop.categories?.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
-      <div className="pagination">
-        <p>Pages</p>
-        {Array.from({ length: Math.ceil(shop.categories?.length / itemsPerPagecategory) }, (_, index) => index + 1).map((pageNumbercategory) => (
-          <button2 key={pageNumbercategory} onClick={() => paginatecategory(pageNumbercategory)}>
-            {pageNumbercategory}
-          </button2>
-        ))}
-      </div>
-      <div className='page'>
-        <main>
-          {selectedCategory && (
-            <>
-              <div>
-                <h4>{shop.categories.find((c) => c.id === selectedCategory)?.name}</h4>
-                <div className="pagination">
-                  <p>Pages</p>
-                  {Array.from({ length: Math.ceil(shop.categories.find((c) => c.id === selectedCategory)?.products.length / itemsPerPage) }, (_, index) => index + 1).map((pageNumber) => (
-                    <button2 key={pageNumber} onClick={() => paginate(pageNumber)}>
-                      {pageNumber}
-                    </button2>
-                  ))}
-                </div>
-                <div className="product-list">
-                  {shop.categories.find((c) => c.id === selectedCategory)?.products
-                    .slice(indexOfFirstItem, indexOfLastItem)
-                    .map((product) => (
-                      <div key={product.id} className="product-card" onClick={() => setProduct(product)} >
-                        {product.image?<img src={product.image} alt={product.name} className='product-image'/>:
-                      <img src="images/defaultproduct.png" alt="not here" className='product-image'/>}
-                        <h5>{product.name}</h5>
-                        <p>Rs.{product.price}</p>
-                      </div>
-                    ))}
-                </div>
+          {product.id ? <div><br /><button3 onClick={goBack}>&lt;</button3> <ShowProduct product={product} do={productchoose} /></div> :
+            <div>
+              <div className="pagination">
+                <p>Pages</p>
+                {Array.from({ length: Math.ceil(shop.categories?.length / itemsPerPagecategory) }, (_, index) => index + 1).map((pageNumbercategory) => (
+                  <button2 key={pageNumbercategory} onClick={() => paginatecategory(pageNumbercategory)}>
+                    {pageNumbercategory}
+                  </button2>
+                ))}
               </div>
-            </>
-          )}
-          {!selectedCategory && shop.categories?.slice(indexOfFirstItemcategory, indexOfLastItemcategory).map((category) => (
-            <div key={category.id}>
-              <h2>{category.name}</h2>
-              <div className='product-list'>
-                {category.products
-                  .slice(indexOfFirstItem, indexOfLastItem)
-                  .map((product) => (
-                    <div key={product.id} className="product-card"  onClick={() => setProduct(product)} >
-                      {product.image?<img src={product.image} alt={product.name} className='product-image'/>:
-                      <img src="images/defaultproduct.png" alt="not here" className='product-image'/>}
-                      <h3>{product.name}</h3>
-                      <span className="price">Rs.{product.price}</span>
+
+              <div className='page'>
+                <main>
+                  {selectedCategory && (
+                    <>
+                      <div>
+                        <h4>{shop.categories.find((c) => c.id === selectedCategory)?.name}</h4>
+                        <div className="pagination">
+                          <p>Pages</p>
+                          {Array.from({ length: Math.ceil(shop.categories.find((c) => c.id === selectedCategory)?.products.length / itemsPerPage) }, (_, index) => index + 1).map((pageNumber) => (
+                            <button2 key={pageNumber} onClick={() => paginate(pageNumber)}>
+                              {pageNumber}
+                            </button2>
+                          ))}
+                        </div>
+                        <div className="product-list">
+                          {shop.categories.find((c) => c.id === selectedCategory)?.products
+                            .slice(indexOfFirstItem, indexOfLastItem)
+                            .map((product) => (
+                              <div key={product.id} className="product-card" onClick={() => setProduct(product)} >
+                                {product.image ? <img src={product.image} alt={product.name} className='product-image' /> :
+                                  <img src="images/defaultproduct.png" alt="not here" className='product-image' />}
+                                <h5>{product.name}</h5>
+                                <p>Rs.{product.price}</p>
+                              </div>
+                            ))}
+                        </div>
+                      </div>
+                    </>
+                  )}
+                  {!selectedCategory && shop.categories?.slice(indexOfFirstItemcategory, indexOfLastItemcategory).map((category) => (
+                    <div key={category.id}>
+                      <h2>{category.name}</h2>
+                      <div className='product-list'>
+                        {category.products
+                          .slice(indexOfFirstItem, indexOfLastItem)
+                          .map((product) => (
+                            <div key={product.id} className="product-card" onClick={() => setProduct(product)} >
+                              {product.image ? <img src={product.image} alt={product.name} className='product-image' /> :
+                                <img src="images/defaultproduct.png" alt="not here" className='product-image' />}
+                              <h3>{product.name}</h3>
+                              <span className="price">Rs.{product.price}</span>
+                            </div>
+                          ))}
+                      </div>
                     </div>
                   ))}
+                </main>
               </div>
+              </div>
+          }
             </div>
-          ))}
-        </main>
       </div>
-    </div>
-    }
-    </>
-  );
+      </>
+      );
 };
 
-export default UserShopPage;
+      export default UserShopPage;
